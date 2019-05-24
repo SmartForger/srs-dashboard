f2c82da9aeda0081e5576211e2b211cfc928898e
SmartForger <23446619+SmartForger@users.noreply.github.com>
2019-05-24 08:23:06 -0700

    fix api url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = process.env.BASE_URL || "http://localhost:8000";
 
 export const getPercentageMonth = () =>
   axios.get(`${BASE_URL}/percentage/month`);
